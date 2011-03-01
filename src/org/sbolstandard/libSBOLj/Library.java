ef4a2fae3813f8913257ba598663b8bcd8d5d8b4
mgaldzic <mgaldzic@gmail.com>
2011-03-01 13:28:19 -0800

    javadocs for Library
    Signed-off-by: mgaldzic <mgaldzic@gmail.com>

--- src/org/sbolstandard/libSBOLj/Library.java
+++ src/org/sbolstandard/libSBOLj/Library.java
@@ -19,8 +19,26 @@ import javax.persistence.OneToMany;
 import org.sbolstandard.libSBOLj.SBOLutil.SkipInJson;
 
 /**
+* The SBOL data model's Library for RDF and Json.
+ *
+ * The objects of this type represent an organizational container which helps
+ * users and developers conceptualize a set of DNA components and SequenceFeatures
+ * as a group. Any combination of these objects can be added to a library,
+ * annotated with a displayID, name and description and be shared on the web.
+ * Such collections could be a set of restriction enzyme recognition
+ * sites, such as the features commonly used for BBF RFC 10 BioBricks. A library
+ * could contain all the DNA components used in a specific project, lab, or any
+ * custom grouping specified by the user.
+ *
+ * COMMENT: Mike Galdzicki thinks Library is not necessary as generic metadata 
+ * element, sets should be left to the lower level implementation (eg Collection in Java).
+ * Or be defined by queries, when there is information that groups the collection.
+ * And new objects should be created and named when the groupings are generally useful.
+ * Having a custom legacy objects for groups will cause downstream development
+ * issues as extensions for useful computable objects are proposed.
  *
  * @author mgaldzic
+ * @version 0.1, 03/01/2011
  */
 @Namespaces({"sbol", "http://sbols.org/sbol.owl#"})
 @RdfsClass("sbol:Library")
@@ -31,9 +49,7 @@ public class Library implements SupportsRdfId {
     private SupportsRdfId mIdSupport = new SupportsRdfIdImpl();
     @RdfId(namespace = "http://sbols.org/sbol.owl#")
     private String id;
-    /**
-     * human readable ID
-     */
+    
     @RdfProperty("sbol:displayId")
     private String displayId;
     @RdfProperty("sbol:name")
@@ -48,13 +64,27 @@ public class Library implements SupportsRdfId {
     private Collection<SequenceFeature> feature = new HashSet<SequenceFeature>();
 
     /**
+     * Elements that are intended as engineering components in synthetic biology.
      *
-     * @return
+     * For example, standard biological parts, BioBricks, oligo components,
+     * vector plasmids, genomes, or any other DNA segment of interest as a building
+     * block of biological systems.
+     *
+     * @return 0 or more <code>DnaComponent</code>[s] that are in this Library
      */
     public Collection<DnaComponent> getComponents() {
         return component;
     }
-
+    /**
+     * Defined DNA segment for engineering biological systems, which belongs to
+     * this Library.
+     *
+     * Any one of the following, standard biological parts, BioBricks, oligo components,
+     * vector plasmids, genomes, or any other DNA segment of interest as a building
+     * block of biological systems.
+     *
+     * @param component a <code>DnaComponent</code> that should be a member of this library
+     */
     void addComponent(DnaComponent component) {
         if (!getComponents().contains(component)) {
             getComponents().add(component);
@@ -62,32 +92,46 @@ public class Library implements SupportsRdfId {
     }
 
     /**
+     * Text which is for users to read and interpret what this Library is.
+     * (eg "Collecting parts which could be used to build honey production directly into
+     * mouse-ear cress"; "T9002 and I7101 variants from Sleight 2010, designs aim to
+     * improve stability over evolutionary time"; "features useful when working with
+     * BBF RFC 10"; "Totally sick Parts I found browsing the web -- SBOL rules
+     * -- organize these later").
      *
-     * @return
+     * Informative description which allows human users to interpret the likely
+     * members of this Library.
+     * @return Human readable text describing the Library
      */
     public String getDescription() {
         return description;
     }
 
     /**
+     * Text with an informative statement about the reason for grouping the Library members.
+     * (eg "Collecting parts which could be used to build honey production directly into
+     * mouse-ear cress"; "T9002 and I7101 variants from Sleight 2010, designs aim to
+     * improve stability over evolutionary time"; "features useful when working with
+     * BBF RFC 10"; "Totally sick Parts I found browsing the web -- SBOL rules
+     * -- organize these later").
      *
-     * @param description
+     * @param description Human readable text describing the Library
      */
     public void setDescription(String description) {
         this.description = description;
     }
 
     /**
-     *
-     * @return
+     * Identifier to display to users.
+     * @return a human readable identifier
      */
     public String getDisplayId() {
         return displayId;
     }
 
     /**
-     *
-     * @param displayId
+     * Identifier to display to users.
+     * @param displayId a human readable identifier
      */
     public void setDisplayId(String displayId) {
         this.displayId = displayId;
@@ -95,13 +139,24 @@ public class Library implements SupportsRdfId {
     }
 
     /**
+     * Descriptions for DNA component sequences.
      *
-     * @return
+     * For example, pBAD, B0015, BioBrick Scar, Insertion Element, "Craig Venter's name".
+     * @return 0 or more <code>SequenceFeatures</code>[s] that are in this Library
      */
     public Collection<SequenceFeature> getFeatures() {
         return feature;
     }
 
+    /**
+     * Defined DNA segment for engineering biological systems, which belongs to
+     * this Library.
+     *
+     * Any one of the following, standard biological parts, BioBricks, oligo components,
+     * vector plasmids, genomes, or any other DNA segment of interest as a building
+     * block of biological systems.
+     * @param feature
+     */
     void addFeature(SequenceFeature feature) {
         if (!getFeatures().contains(feature)) {
             getFeatures().add(feature);
@@ -109,15 +164,15 @@ public class Library implements SupportsRdfId {
     }
 
     /**
-     *
-     * @return
+     * The ID portion of the URI which identifies this object
+     * @return the RDF id for the object
      */
     public String getId() {
         return id;
     }
 
     /**
-     *
+     * A unique identifier which will be used as the ID portion of the URI
      * @param id
      */
     public void setId(String id) {
@@ -125,24 +180,28 @@ public class Library implements SupportsRdfId {
     }
 
     /**
-     *
-     * @return
+     * Recognizable human identifier, it is often ambiguous.(eg. Mike's Arabidopsis Project A;
+     * Sleight, et al. (2010) J.Bioeng; BBF RFC 10 features; Bookmarked Parts).
+     * @return its name, commonly used to refer to this Library
      */
     public String getName() {
         return name;
     }
 
     /**
-     *
-     * @param name
+     * Common name of Library should confer what is contained in the Library.
+     *(eg. Mike's Arabidopsis Project A;
+     * Sleight, et al. (2010) J.Bioeng; BBF RFC 10 features; Bookmarked Parts).
+     * @param name commonly used to refer to this Library (eg. Project A)
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
-     *
-     * @return
+     * a com.clarkparsia.empire required RDF id.
+     * Not sure what the difference is in empire compared to {@link id}
+     * @return the RdfId component of the URI for the DnaComponent
      */
     public RdfKey getRdfId() {
         return mIdSupport.getRdfId();
@@ -150,7 +209,7 @@ public class Library implements SupportsRdfId {
 
     /**
      *
-     * @param id
+     * @param id //note use .setID instead for now, it takes string
      */
     public void setRdfId(final RdfKey id) {
         mIdSupport.setRdfId(id);
