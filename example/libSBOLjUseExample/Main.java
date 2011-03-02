acb5a73f46e1fe43af8e573d7f85edaf7d67ff1f
mgaldzic <mgaldzic@gmail.com>
2011-03-01 23:27:04 -0800

    Main javadoc in source for example/ Signed-off-by: mgaldzic <mgaldzic@gmail.com>

--- example/libSBOLjUseExample/Main.java
+++ example/libSBOLjUseExample/Main.java
@@ -10,16 +10,22 @@ import org.sbolstandard.libSBOLj.SBOLutil;
 import org.sbolstandard.libSBOLj.Library;
 
 /**
+ * Main serves as a simple example of use.
+ *
+ * It is a convenient place for a simple example, when developing something new.
+ * TODO: make a completely separate examples of use code base.
  *
  * @author mgaldzic
+ * @version 0.1, 02/10/2011
  */
 public class Main {
 
     /**
+     * Run this to see what SBOL does now.
      * @param args the command line arguments
      */
     public static void main(String[] args) throws BioException {
-        System.out.println("createDnaComponent");
+        System.out.println("createLibrary");
         SBOLutil s = new SBOLutil();
 
         RichSequenceIterator aRsIter = s.fromGenBankFile("test\\test_files\\BFa_8.15.gb");
