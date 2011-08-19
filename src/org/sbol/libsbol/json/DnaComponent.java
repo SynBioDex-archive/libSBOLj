9aa29dbaf4e3379e392e5df6f6711355e1fe677f
Cesar A. Rodriguez <one.seminole@gmail.com>
2011-08-18 18:17:50 -0700

    Renamed the org.sbol.libsbolj.json package to org.sbol.libsbol.json.

---
+++ src/org/sbol/libsbol/json/DnaComponent.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sbol.libsbol.json;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author cesarr
+ */
+public class DnaComponent
+{
+    protected String                        displayID;
+    protected String                        name;
+    protected String                        description;
+    protected String                        type;
+    protected DnaSequence                   dnaSequence;
+    protected ArrayList<SequenceAnnotation> annotations;
+
+    public DnaComponent(String displayId, String name, String description, String type)
+    {
+        this.displayID = displayId;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.dnaSequence = null;
+    }
+    
+    public DnaComponent(String displayId, String name, String description, String type, DnaSequence dnaSequence)
+    {
+        this.displayID = displayId;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.dnaSequence = dnaSequence;
+    }
+
+    /**
+     * @return the displayId
+     */
+    public String getDisplayID()
+    {
+        return displayID;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType()
+    {
+        return type;
+    }
+
+    /**
+     * @return the DnaSequence
+     */
+    public DnaSequence getDnaSequence()
+    {
+        return dnaSequence;
+    }
+
+    public ArrayList<SequenceAnnotation> getAnnotations()
+    {
+        return annotations;
+    }
+
+    public void setAnnotations(ArrayList<SequenceAnnotation> annotations)
+    {
+        this.annotations = annotations;
+    }
+}
